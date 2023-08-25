03b63d0a5aafe2f965de28867ab19ec1c5b523d4
Reece Jocumsen <reecejocumsen@gmail.com>
2023-08-25 13:12:33 +1000

    The first bit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,30 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+opiod_analgesics_to_morphine_data = [
+    ("Codeine", "PO", 10),
+    ("Diamorphine", "IV", 0.33),
+    ("Diamorphine", "SC", 0.33),
+    ("Dihydrocodeine",  "PO", 10),
+    ("Hydromorphone", "PO", 0.2),
+    ("Morphine", "IV", 0.5),
+    ("Morphine", "SC", 0.5),
+    ("Oxycodone", "PO", 0.66),
+    ("Oxycodone", "IV", 0.5),
+    ("Oxycodone", "SC", 0.5),
+    ("Tramadol", "PO", 10)
+]
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.title("Opioptions")
+st.header("Equivalent doses of opiod analgesics to oral morphine")
+st.markdown("""This table is an **approximate** guide; patients should be 
+            carefully monitored after any change in medication and dose 
+            titration may be required. Conversion ratios vary in the literature;
+            these conversion ratios are based on the Palliative Care Formulary 
+            8th edition.""")
+oral_morphine_dose = st.number_input("Oral morphine sulfate dose (mg)")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if oral_morphine_dose != 0:
+    st.dataframe([{'Analgesic': drug[0], 'Route': drug[1], f'Equivalent dose to {oral_morphine_dose}mg of oral morphine sulfate in mg': drug[2]*oral_morphine_dose} for drug in opiod_analgesics_to_morphine_data], use_container_width=True)
+    st.caption("PO = by mouth; IV = intravenous; SC = subcutaneous")
+    st.caption("**NOTE: 10mg oral oxycodone is approximately equivalent to 6.6mg IV/SC oxycodone.**")
+st.divider()
